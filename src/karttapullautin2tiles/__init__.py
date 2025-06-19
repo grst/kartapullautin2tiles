872f77dc964a3baad2911ce047766a1002288900
Gregor Sturm <mail@gregor-sturm.de>
2025-06-19 20:53:41 +0200

    Fix min zoom level

--- src/karttapullautin2tiles/__init__.py
+++ src/karttapullautin2tiles/__init__.py
@@ -310,7 +310,7 @@ def make_tiles(
         img_array, img_transform = rasterio.merge.merge(
             tmp_df["img_file"], bounds=tuple(tmp_df.total_bounds), nodata=255, dtype=np.uint8
         )
-        for zoom in range(parent_tile.z + 1, max_zoom + 1):
+        for zoom in range(parent_tile.z, max_zoom + 1):
             for tile in mercantile.children(parent_tile, zoom=zoom):
                 img = extract_and_transform_tile(img_array, img_transform, tile, src_crs=gpdf.crs)
 
